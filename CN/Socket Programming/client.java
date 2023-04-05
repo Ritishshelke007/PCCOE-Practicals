1e03d11c1fa96f6f0600847cea15c65f8d14e314
Ritish Shelke <89585029+Ritishshelke007@users.noreply.github.com>
2023-04-05 12:58:16 +0530

    Update client.java

--- CN/Socket Programming/client.java
+++ CN/Socket Programming/client.java
@@ -5,19 +5,24 @@
     class client{  
     public static void main(String args[])throws Exception{  
 
+    // at client side socket is necessary
     Socket s = new Socket("SERVER_IP_ADDRESS",PORT_NUMBER);
 
+    // for both read and write set streams
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-
+    // set a buffered reader 
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
       
+    // initialize strings to store read and write message     
     String str="",str2="";  
     while(!str.equals("stop")){  
         str=br.readLine();  
         dout.writeUTF(str);  
  
         dout.flush();
+        
+     // handle exception when stop is receeived 
         try{
         str2=din.readUTF();  
         System.out.println("Message from Server : "+str2); 
