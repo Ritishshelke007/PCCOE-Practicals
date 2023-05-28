0a34f3cfabf4474940614d89d32dff24bed57b33
Ritish Shelke <89585029+Ritishshelke007@users.noreply.github.com>
2023-05-28 23:13:54 +0530

    Update Server.java

--- CN/Go Back N/Server.java
+++ CN/Go Back N/Server.java
@@ -1,69 +1,78 @@
+import java.util.*;
+import java.io.*;
 import java.net.*;
-import java.io.*;  
-import java.lang.*;
 
-class Server{  
-public static void main(String args[])throws Exception{ 
-    
-int n;
-int data=0;
-int check=0;
-// int rec_frame_data[] = new int[5];
-// int rec_data;
 
-ServerSocket ss=new ServerSocket(3334);  
-Socket s=ss.accept();  
+public class Server {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int i=0;
 
-System.out.println("Client connected!");
+        ServerSocket ss = new ServerSocket(4444);
+        System.out.println("=======Server=======");
+        Socket s = ss.accept();
+        System.out.println("Client connected");
 
-DataInputStream din=new DataInputStream(s.getInputStream());  
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+        DataInputStream in = new DataInputStream(s.getInputStream());
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-n  = din.read();
-System.out.println("No of frames you will be recieving from client are : "+n);
-System.out.println("---------------Ready to receive frames------------\n");
-
-for(int i=0; i<n; i++){
-data = din.read();
-    if(check==data){
-        System.out.println("Received Frame no : "+ data);
-        System.out.println("Acknoledgment : " + (i+1));
-        dout.write(i+1);
-        check++;
-    }
-    else{
-        System.out.println("Discarded frame no : "+i);
-        System.out.println("Sending negative ack : " +(i-1));
+        int frames = in.read();
+        System.out.println("No of frames to send : "+frames);
         
-        check++;
-    }
-   // Thread.sleep(1200);
-}
+        boolean status[] = new boolean[frames]; 
 
-// if(i==(n-1)){
-//     System.out.println("-----All frames are received!-----");
-// }
-// else{
-//     System.out.println("May one or more frames are list");
-// }
+        int choice = in.read();
 
-// System.out.println("Receiving again----");
+        if(choice==0){
 
-// for(int i=0; i<n; i++){
-//     rec_frame_data[i]  = din.read();
-//     // Thread.sleep(1200);
-//     System.out.println("Frame no : "+ i + " Received data : "+ rec_frame_data[i]);
-// }
+            for(i=0;i<frames;i++){
+                System.out.println("Sending frame no : "+i);
+                out.write(i);
+                out.flush();
+                int j = in.read();
+                System.out.println("ACK received for frame "+i+" as "+j);
+                System.out.println("\n");
+            }
+        }
+        else{
+            for(i=0;i<frames;i++){
+
+                if(i==2){
+                    System.out.println("Sending frame no : "+i);
+                }
+                else{
+                    System.out.println("Sending frame no : "+i);
+                    out.write(i);
+                    out.flush();
+                    int j = in.read();
+
+                    if(j!=255){
+
+                        System.out.println("ACK received for frame "+i+" as "+j);
+                        status[i] = true;
+                        System.out.println("\n");
+                    }
+                    else{
+                        System.out.println("Received Negative ACK");
+                        status[i] = false;
+                    }
+                }
+            }
+            // resending frames 
+            for(i=0;i<frames;i++){
+                if(status[i]==false){
+                    System.out.println("\nResending frame no : "+i);
+                    out.write(i);
+                    out.flush();
+                    int j = in.read();
+                    System.out.println("ACK received for frame "+i+" as "+j);
+                    status[i]=true;
+                }
+            }
+        }
 
 
-// int changed_add = din.read();
-// System.out.println(changed_add);
 
-// int changed_val = din.read();
-// System.out.println("Changed to : "+changed_val);
-
-din.close();  
-s.close();  
-ss.close();  
-}}  
+    } // end main
+    
+} // end class
