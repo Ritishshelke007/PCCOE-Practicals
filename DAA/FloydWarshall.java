470746e9418b0a004e12de5238c0d0ad9fea0195
Ritish Shelke <89585029+Ritishshelke007@users.noreply.github.com>
2023-11-23 02:14:00 +0530

    Update FloydWarshall.java

--- DAA/FloydWarshall.java
+++ DAA/FloydWarshall.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /*
 INPUT:
 4
@@ -30,78 +29,99 @@ output:
 2 5 7 0
 */
 
+
 public class FloydWarshall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int infinity = Integer.MAX_VALUE;
+        System.out.println("Total vertices : ");
+        int v = sc.nextInt();
 
-        // System.out.print("Enter no of vertices : ");
-        // int n = sc.nextInt();
+        int[][] matrix = new int[v][v];
 
-        // System.out.print("No of edges : ");
-        // int edges = sc.nextInt();
-        // int[][] matrix = {
-        //     {0,3,infinity,7},
-        //     {8,0,2,infinity},
-        //     {5,infinity,0,1},
-        //     {2,infinity,infinity,0}
-        // };
-
-        int[][] matrix = {
-            {0,8,infinity,1},
-            {infinity,0,1,infinity},
-            {4,infinity,0,infinity},
-            {infinity,2,9,0}
-        };
-
-        for(int i=0;i<4;i++){
-            for(int j=0; j<4;j++){
-                if(matrix[i][j]==infinity){
-                    System.out.print("∞" + " ");
+        System.out.println("Enter matrix (input -1 if dist is inf)");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                matrix[i][j] = sc.nextInt();
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = Integer.MAX_VALUE;
                 }
-                else{
-                    System.out.print(matrix[i][j] + " ");
-                }
+            }
+        }
+
+        // input matrix
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (matrix[i][j] == Integer.MAX_VALUE)
+                    System.out.print("inf" + "\t");
+                else
+                    System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
         }
 
-        // shortest distance from intermediate nodes
-        findDist(matrix,4,infinity);
-
-    }
-    static void findDist(int[][] matrix,int v,int infinity){
         int[][] result = new int[v][v];
+        int[][] path = new int[v][v];
 
-        for(int i=0;i<v;i++){
-            for(int j=0;j<v;j++){
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
                 result[i][j] = matrix[i][j];
+                path[i][j] = -1;
             }
         }
 
-        for(int i=0; i<v;i++){
-            for(int j=0;j<v;j++){
-                for(int k=0;k<v;k++){
-                    if (result[j][i]<infinity && result[i][k]<infinity) {
-                        if(result[j][k] >= result[j][i]+result[i][k]){
-                            result[j][k] = result[j][i]+result[i][k];
+        // logic
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                for (int k = 0; k < v; k++) {
+                    if (result[j][i] < Integer.MAX_VALUE && result[i][k] < Integer.MAX_VALUE) {
+                        if (result[j][k] > result[j][i] + result[i][k]) {
+                            result[j][k] = result[j][i] + result[i][k];
+                            path[j][k] = i;
                         }
                     }
                 }
             }
-            System.out.println("Matrix after intermediate node : "+i);
-            for(int a=0;a<v;a++){
-                for(int b=0;b<v;b++){
-                    if(result[a][b]==infinity){
-                        System.out.print("∞" + " ");
+            System.out.println("Resultant matrix after intermediate node : " + (i + 1));
+            for (int a = 0; a < v; a++) {
+                try {
+                    for (int b = 0; b < v; b++) {
+
+                        System.out.print(result[a][b] + "\t");
                     }
-                    else{
-                        System.out.print(result[a][b] + " ");
-                    }
+                    System.out.println();
+                } catch (Exception e) {
+                    System.out.println(e);
                 }
-                System.out.println("");
             }
         }
 
+        System.out.println("Final matrix");
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (result[i][j] == Integer.MAX_VALUE)
+                    System.out.print("inf" + "\t");
+                else
+                    System.out.print(result[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        int u1 = 0;
+        int v1 = 1;
+
+        int minDist = result[u1][v1];
+        System.out.println("Min Dist between " + u1 + " and " + v1 + " is " + minDist);
+
+        System.out.print("Path: ");
+        printPath(path, u1, v1);
+
     }
+    static void printPath(int[][] path, int u1, int v1){
+        if (path[u1][v1] == -1) {
+            System.out.print(u1 + " -> " + v1);
+            return;
+        }
+    printPath(path, u1, path[u1][v1]);
+    System.out.print(" -> " + v1); 
+  } 
 }
